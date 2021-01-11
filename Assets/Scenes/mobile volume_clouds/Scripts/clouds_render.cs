f1472a86673e105a783468b201219f35c5d78275
zero-gz <hust_sdxz@163.com>
2021-01-11 10:00:23 +0800

    modify the mobile clouds render

--- Assets/Scenes/mobile volume_clouds/Scripts/clouds_render.cs
+++ Assets/Scenes/mobile volume_clouds/Scripts/clouds_render.cs
@@ -1,25 +1,40 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class clouds_render : MonoBehaviour
 {
     Camera camera;
     Material _UpscaleMaterial;
-    public RenderTexture clouds_rt;
     public float small_scale = 2.0f;
-    public Texture2D black_tex;
-    // Start is called before the first frame update
-    void Start()
-    {
-        clouds_rt = new RenderTexture((int)(Screen.width / small_scale), (int)(Screen.height / small_scale), 24, RenderTextureFormat.Default);
-        clouds_rt.name = "clouds_rt";
 
+    private CommandBuffer cmd = null;
+    private static int _CLOUDS_RT_ID = Shader.PropertyToID("CloudsRT");
+
+    // Start is called before the first frame update
+    void Awake()
+    {
         camera = GetComponent<Camera>();
-        //camera.targetTexture = clouds_rt;
 
         _UpscaleMaterial = new Material(Shader.Find("Hidden/CloudBlender"));
         _UpscaleMaterial.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    void OnEnable()
+    {
+        cmd = new CommandBuffer();
+        cmd.name = "TestInstance";
+        camera.AddCommandBuffer(CameraEvent.BeforeImageEffectsOpaque, cmd);
+    }
+
+    private void OnDisable()
+    {
+        if (cmd != null)
+        {
+            camera.RemoveCommandBuffer(CameraEvent.BeforeImageEffectsOpaque, cmd);
+            cmd = null;
+        }
     }
 
     // Update is called once per frame
@@ -27,13 +42,17 @@ public class clouds_render : MonoBehaviour
     {
 
     }
-
-    private void OnPostRender()
+    private void OnPreRender()
     {
-        RenderTexture activeRT = RenderTexture.active;
-        RenderTexture.active = clouds_rt;
+        RenderClouds();
+    }
 
-        Graphics.Blit(black_tex, clouds_rt);
+    private void RenderClouds()
+    {
+        if (cmd == null)
+            return;
+
+        cmd.Clear();
 
         // 获取clouds层的所有GameObject
         var obs = GenerateClouds.cloudsObjects;
@@ -42,24 +61,25 @@ public class clouds_render : MonoBehaviour
         Matrix4x4[] mat_list = new Matrix4x4[num];
         GameObject tmp = obs[0];
 
-        Mesh mesh = tmp.GetComponent<MeshFilter>().mesh; ;
-        Material mtl = tmp.GetComponent<MeshRenderer>().material;
+        Mesh mesh = tmp.GetComponent<MeshFilter>().sharedMesh;
+        Material mtl = tmp.GetComponent<MeshRenderer>().sharedMaterial;
         mtl.SetPass(0);
         for (int i = 0; i != num; i++)
         {
             GameObject ob = obs[i];
             Matrix4x4 mat = ob.transform.localToWorldMatrix;
             mat_list[i] = mat;
-            //Graphics.DrawMeshNow(mesh, mat, 0);
         }
 
-        Graphics.DrawMeshInstanced(mesh, 0, mtl, mat_list);
-        RenderTexture.active = activeRT;
+        cmd.GetTemporaryRT(_CLOUDS_RT_ID, (int)(Screen.width/small_scale), (int)(Screen.height/small_scale), 0, FilterMode.Bilinear, RenderTextureFormat.DefaultHDR);
+        cmd.SetRenderTarget(_CLOUDS_RT_ID);
+        cmd.ClearRenderTarget(true, true, new Color(0, 0, 0, 0));
+        cmd.SetGlobalTexture(Shader.PropertyToID("_Clouds"), _CLOUDS_RT_ID);
+        cmd.DrawMeshInstanced(mesh, 0, mtl, 0, mat_list, num);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        _UpscaleMaterial.SetTexture("_Clouds", clouds_rt);
         Graphics.Blit(source, destination, _UpscaleMaterial, 0);
     }
 }
